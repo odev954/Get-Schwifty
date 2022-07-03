f5d8987fb29626549a035ce5e72e76d47102e99a
orel aharonov <orel@invarial.com>
2022-07-03 19:14:39 +0300

    added input checks to swap method

--- Model/Board.js
+++ Model/Board.js
@@ -13,11 +13,30 @@ class Board {
         }
     }
 
-    // swap between elements
-    // gets indexes in format {row: x, column: y}
     swap(source, destination) {
-        let temporary = this.board[destination.row][destination.column];
-        this.board[destination.row][destination.column] = this.board[source.row][source.column]
-        this.board[source.row][source.column] = temporary
+        if('row' in source &&
+           'row' in destination &&
+           'column' in source && 
+           'column' in destination)
+        {
+            //check range
+            if(this.board.length > destination.row && 
+               this.board[destination.row].length > destination.column && 
+               this.board.length > source.row && 
+               this.board[source.row].length > source.column)
+            {
+                let temporary = this.board[destination.row][destination.column];
+                this.board[destination.row][destination.column] = this.board[source.row][source.column]
+                this.board[source.row][source.column] = temporary
+            }
+            else
+            {
+                throw new RangeError("Invalid source / destination")
+            }
+        }
+        else
+        {
+            throw new TypeError("Indexes should be in format {row: x, column: y}")
+        }
     }
 }
